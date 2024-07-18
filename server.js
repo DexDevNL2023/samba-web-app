3becd5a317e15ff68c921254b0268f2c719ed6f8
DexDevNL2023 <dexternlang@gmail.com>
2024-07-18 21:58:04 +0200

    dex39

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 8080;
+
 const app = express();
 
-app.use(express.static(__dirname));
-app.use(express.static(__dirname + '/dist/samba-web-app'))
+// Serve static files from the dist directory
+app.use(express.static(path.join(__dirname, 'dist/samba-web-app')));
 
-app.get('/ping', function (req, res)
-{
-    return res.send('pong');
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/samba-web-app/index.html'));
 });
 
-app.get('/*', function (req, res)
-{
-    res.sendFile(path.join(__dirname + '/dist/samba-web-app/index.html'));
-});
-
-app.listen(port, function ()
-{
-    console.info('Angular Server App listening on port ' + port);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
