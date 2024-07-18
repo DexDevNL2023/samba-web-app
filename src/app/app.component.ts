36a2548554b6cf05d4bc902ac45b775adf7eb2cf
DexDevNL2023 <dexternlang@gmail.com>
2024-07-18 22:45:40 +0200

    dex44

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     menuMode = 'slim';
 
     inputStyle = 'outlined';
@@ -22,5 +22,21 @@ export class AppComponent {
     ngOnInit() {
         this.primengConfig.ripple = true;
         this.ripple = true;
+        this.loadStyle('/assets/theme/theme-teal-yellow.css', 'theme-css');
+        this.loadStyle('/assets/layout/css/layout-teal-yellow.css', 'layout-css');
+    }
+
+    loadStyle(href: string, id: string) {
+      const existingLink = document.getElementById(id) as HTMLLinkElement;
+      if (existingLink) {
+        existingLink.href = href;
+      } else {
+        const head = document.getElementsByTagName('head')[0];
+        const link = document.createElement('link');
+        link.id = id;
+        link.rel = 'stylesheet';
+        link.href = href;
+        head.appendChild(link);
+      }
     }
 }
