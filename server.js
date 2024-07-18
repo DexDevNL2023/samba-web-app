81d479b5ea9c9a6e31e2dec236a979eae5d422ad
DexDevNL2023 <dexternlang@gmail.com>
2024-07-18 15:39:05 +0200

    dex11

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname));
-app.use(express.static(__dirname + '/dist/samba-web-app'))
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/samba-web-app'));
 
-app.get('/ping', function (req, res)
-{
-    return res.send('pong');
-});
- 
-app.get('/*', function (req, res)
-{
-    res.sendFile(path.join(__dirname + '/dist/samba-web-app/index.html'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/samba-web-app/index.html'));
 });
 
-app.listen(port, function ()
-{
-    console.info('Angular Server App listening on port ' + port);
-});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
