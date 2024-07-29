c9e288bb65123243d284cb24701e909e06972009
DexDevNL2023 <dexternlang@gmail.com>
2024-07-29 08:48:20 +0100

    dex84c

--- src/app/models/souscription.model.ts
+++ src/app/models/souscription.model.ts
@@ -39,6 +39,7 @@ export interface Souscription extends BaseEntity {
   numeroSouscription?: string | null;
   dateSouscription?: Date | null;
   dateExpiration?: Date | null;
+  montantCotisation?: number | null;
   status?: keyof typeof SubscriptionStatus | null;
   frequencePaiement?: keyof typeof PaymentFrequency | null;
   assure?: number | null;
