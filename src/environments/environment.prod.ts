e02f4d131881ba882741e6ab826a70da9fdd59a7
DexDevNL2023 <dexternlang@gmail.com>
2024-10-04 20:28:10 -0700

    dex2120000

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://samba-web-api-d166bd1d7307.herokuapp.com/',  // URL de l'API pour l'environnement de production
+  apiUrl: 'https://samba-web-api-c853a739dee0.herokuapp.com/',  // URL de l'API pour l'environnement de production
   logging: false,  // Désactivation du logging en production
   secretExcel: "2+2Font4",
 };
