f3e4da3f7fb79e10e3c44fac2e04a4aa7329dd66
Nlang Essama Victor Dexter <146982827+DexDevNL2023@users.noreply.github.com>
2024-10-22 12:45:34 -0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://samba-web-api-c9e839abd962.herokuapp.com',  // URL de l'API pour l'environnement de production
+  apiUrl: 'https://samba-web-api-v1-dbbfa67e2b93.herokuapp.com',  // URL de l'API pour l'environnement de production
   logging: false,  // Désactivation du logging en production
   secretExcel: "2+2Font4",
 };
